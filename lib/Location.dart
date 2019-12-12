d7ea3541400d5c639ac0eb773c27cc3d43072d75
hasanm08 <hasanmahani08@gmail.com>
2019-12-12 13:28:02 +0330

    location3

--- lib/Location.dart
+++ lib/Location.dart
@@ -1,28 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_images_slider/flutter_images_slider.dart';
 
-
 class Location extends StatelessWidget {
- // List<serviceCard> servs;
-  String  title ;
+  // List<serviceCard> servs;
+  String title;
   String description;
   String addrees;
   int rating;
-  Location({this.addrees,this.description,this.rating,this.title});
-  static Widget top = ImagesSlider(
-      items: map<Widget>(imgs, (index, i) {
-    return Container(
-      decoration: BoxDecoration(
-          image: DecorationImage(image: NetworkImage(i), fit: BoxFit.cover)),
-    );
-  }));
+  Location({this.addrees, this.description, this.rating, this.title});
+   Widget top = Stack(
+    children: <Widget>[
+      ImagesSlider(
+          items: map<Widget>(imgs, (index, i) {
+        return Container(
+          decoration: BoxDecoration(
+              image:
+                  DecorationImage(image: NetworkImage(i), fit: BoxFit.cover)),
+        );
+      })),
+      Positioned(left: 5,top: 5,
+      child: Container(
+        width: 30,
+        height: 20,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          color: Colors.black.withOpacity(.5)
+        ),
+       // child: Text(rating.toString()),//TODO rating
+      ),)
+    ],
+  );
 
   static List<String> imgs = [
     "https://via.placeholder.com/1600x900",
     "https://via.placeholder.com/1600x900"
   ];
 
-  
   static List<T> map<T>(List list, Function handler) {
     List<T> result = [];
     for (var i = 0; i < list.length; i++) {
@@ -30,20 +43,24 @@ class Location extends StatelessWidget {
     }
     return result;
   }
- 
+
   Widget services = SingleChildScrollView(
     child: Column(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       mainAxisSize: MainAxisSize.max,
       children: [
-  serviceCard(cost: 1500,name: "تست",describe: "جهت تست",)
-],
+        serviceCard(
+          cost: 1500,
+          name: "تست",
+          describe: "جهت تست",
+        )
+      ],
     ),
   );
 
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
+    return Scaffold(
       //appBar: AppBar(title: Text("محل",textDirection: TextDirection.rtl,),
       //centerTitle: true,),
       body: Column(
@@ -53,26 +70,46 @@ class Location extends StatelessWidget {
           top,
           //services,
           //TODO Background
-          Text("عنوان : "+title,textDirection: TextDirection.rtl,style: TextStyle(color: Colors. black,fontSize: 20,),),
-          Text("توضیحات : "+description,textDirection: TextDirection.rtl,style: TextStyle(color: Colors. black,fontSize: 20,)),
-          Text("آدرس : "+ addrees,textDirection: TextDirection.rtl,style: TextStyle(color: Colors. black,fontSize: 20,)),
+          Text(
+            "عنوان : " + title,
+            textDirection: TextDirection.rtl,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+            ),
+          ),
+          Text("توضیحات : " + description,
+              textDirection: TextDirection.rtl,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+              )),
+          Text("آدرس : " + addrees,
+              textDirection: TextDirection.rtl,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+              )),
           FlatButton(
             child: Container(
               width: 150,
               padding: EdgeInsets.all(8),
               decoration: BoxDecoration(
-                color: Colors.black54,
+                  color: Colors.black54,
                   borderRadius: BorderRadius.all(Radius.circular(18))),
-              child: Center(child: Text("رزرو",style: TextStyle(color: Colors.white,fontSize: 15),)),
+              child: Center(
+                  child: Text(
+                "رزرو",
+                style: TextStyle(color: Colors.white, fontSize: 15),
+              )),
             ),
-            onPressed: (){
+            onPressed: () {
               //TODO reserve
             },
           )
         ],
       ),
     );
-  
   }
 }
 
@@ -87,9 +124,8 @@ class serviceCard extends StatelessWidget {
       height: 150,
       width: 180,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(8)),
-        color: Colors.black38
-      ),
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+          color: Colors.black38),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         mainAxisSize: MainAxisSize.max,
